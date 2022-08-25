d97e7972ded98265f385378166a3d42fb589914c
Ezenwa Hopekell <hopekelltech@gmail.com>
2022-08-25 22:39:51 +0100

    Added Withdrawal Log

--- views/user/withdraw.php
+++ views/user/withdraw.php
@@ -1,4 +1,4 @@
-<div class="row">
+          <div class="row">
             <div class="col-md-3 col-sm-6 col-12">
                 <div class="info-box">
                 <span class="info-box-icon bg-info"><i class="fas fa-wallet"></i></span>
@@ -34,4 +34,65 @@
             <!-- /.card -->
             </div>
             <!-- ./col -->
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h5>Withdrawal Log</h5>
+              </div>
+              <div class="card-body">
+                <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                      <th>TX ID</th>
+                      <th>Amount</th>
+                      <th>Method</th>
+                      <th>Status</th>
+                      <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $result = $conn->query("SELECT * FROM withdrawals WHERE user_id = '$id' ORDER BY id DESC");
+                    if ($result->num_rows > 0) {
+                      while ($trow = $result->fetch_assoc()) {
+                        extract($trow);
+                        ?>
+                    <tr>
+                      <td>#<?php echo $ref;?></td>
+                      <td><?php echo $app->currency_symbol . number_format($amount,0,'.',',');?></td>
+                      <td>
+                        <?php $method; ?>
+                      </td>
+                      <td>
+                        
+                      <?php
+                        if ($status =='1') {
+                        echo ' <span class="badge badge-success">Success</span>';
+                        }else{
+                          echo ' <span class="badge badge-warning">Pending</span>';
+                        }
+                      ?>
+                      </td>
+                      <td><?php echo date('F d, Y (g:iA)', strtotime($updated_at));?></td>
+                    </tr>
+                        <?php
+                      }
+                    }
+                    ?>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                      <th>TX ID</th>
+                      <th>Amount</th>
+                      <th>Method</th>
+                      <th>Status</th>
+                      <th>Date</th>
+                    </tr>
+                    </tfoot>
+                  </table>
+              </div>
+            </div>
+          </div>
         </div>
