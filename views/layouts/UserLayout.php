0b11c52afa8e63a58708d503f23ef191d6c8f1e8
Ezenwa Hopekell <hopekelltech@gmail.com>
2022-08-26 00:20:04 +0100

    Added Profile Picture

--- views/layouts/UserLayout.php
+++ views/layouts/UserLayout.php
@@ -83,7 +83,7 @@ CheckWallet($id,$reg_bonus);
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="./assets/dist//img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php if ($avatar!=null) { echo $avatar;}else { echo './assets/dist/img/avatar.png';} ?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo $name;?></a>
